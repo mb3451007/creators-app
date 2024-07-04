9aa2800219cd7b7562d5e054b249de70282addc4
hameedrana <0hameedrana@gmail.com>
2024-07-04 21:09:02 +0500

    messages page

--- src/app/component/messages/messages.component.ts
+++ src/app/component/messages/messages.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent {
+  selectedDiv: number = 0;
+  showHide: boolean = true;
 
+  activeDiv: number = 1; // By default, the first div is active
+  
+  selectDiv(divNumber: number): void {
+    this.selectedDiv = divNumber;
+  }
+  setActiveDiv(divNumber: number) {
+    this.activeDiv = divNumber;
+  }
+  chatHide() {
+    this.showHide = true;
+  }
+
+  mediaHide() {
+    this.showHide = false;
+  }
 }
