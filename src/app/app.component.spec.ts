3f6214c4dd42ca29b6288697de3cda00b6ea58bc
hameedrana <0hameedrana@gmail.com>
2024-09-15 17:33:35 +0500

    fanvue replace with OBO

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fanvue'`, () => {
+  it(`should have as title 'OBO'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fanvue');
+    expect(app.title).toEqual('OBO');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fanvue app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('OBO app is running!');
   });
 });
