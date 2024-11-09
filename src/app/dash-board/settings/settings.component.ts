bdfd0f03046092129dc8fd5fb1afc04be9b43392
hameedrana <0hameedrana@gmail.com>
2024-11-09 15:23:05 +0500

    setting page set on mobile view

--- src/app/dash-board/settings/settings.component.ts
+++ src/app/dash-board/settings/settings.component.ts
@@ -3,11 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
+  
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {
   selectedDiv: number = 0;
   showDetails: boolean = false;
+
+  // mobile viewport
+  selectedDivModile: number | null = null;
+
+  selectDivMobileport(divNumber: number) {
+    this.selectedDivModile = divNumber;
+  }
+
+  goBack() {
+    this.selectedDivModile = null;
+  }
+  // 
 
   selectDiv(divNumber: number): void {
     this.selectedDiv = divNumber;
