2e81d0231a58690ea20c6dc908c677e7d7f5c824
Mohsin Zafar <heykhattak@gmail.com>
2025-04-30 11:30:04 +0500

    .env changed

--- src/app/environments/env.development.ts
+++ src/app/environments/env.development.ts
@@ -1,8 +1,8 @@
 export const env = {
   production: false,
-  baseURL: 'http://localhost:3000',
+  // baseURL: 'http://localhost:3000',
   stripeKey:
     'pk_test_51QnH6BPiAjowyLOGOVBX80WL9OmLX5COcAVKCj7bdkVjLg93nHv3gq9a1wvW3pwiPXPKpmQEdXu5B6ZgKkYm693Z00Z0qoTWlR',
 
-  // baseURL: 'https://oboplatform.com:3000',
+  baseURL: 'https://oboplatform.com/api',
 };
